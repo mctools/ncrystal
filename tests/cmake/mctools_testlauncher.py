a1582c3b48d69c4c532c1687e1ba038a371ea280
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-09-17 10:03:06 +0200

    More verbose testlauncher.py output

--- tests/cmake/mctools_testlauncher.py
+++ tests/cmake/mctools_testlauncher.py
@@ -5,6 +5,7 @@
 
 import pathlib
 import shutil
+import shlex
 
 def run( app_file, reflogfile = None ):
     wd=pathlib.Path('./wd')
@@ -15,14 +16,21 @@ def run( app_file, reflogfile = None ):
     cmd = [str(app_file)]
     if app_file.name.endswith('.py'):
         cmd = [sys.executable] + cmd
+    print("MCTools TestLauncher running command:")
+    for e in cmd:
+        print(f"  {shlex.quote(e)}")
+    print()
+    sys.stdout.flush()
     r = subprocess.run( cmd,
                         capture_output = True,
                         cwd = wd )
+    print("MCTools TestLauncher done running command.")
     if r.stderr:
         #Todo support this, by merging them (probably needs subprocess.Popen not
         #subprocess.run):
         for line in r.stderr.splitlines():
             sys.stderr.buffer.write(b'stderr> '+line+'\n'.encode())
+        sys.stderr.flush()
         raise SystemExit('Error: Process emitted output on'
                          ' stderr (not supported yet with ref logs)')
     output_raw = r.stdout
@@ -37,14 +45,16 @@ def run( app_file, reflogfile = None ):
         return #Done!
     refoutput = reflogfile.read_bytes()
     if output_raw == refoutput:
+        sys.stdout.flush()
         print("Reference log-files are exact byte-for-byte match!")
+        sys.stdout.flush()
         return
     output = output_raw.decode().splitlines()
     refoutput = refoutput.decode().splitlines()
     if output == reflogfile:
+        sys.stdout.flush()
         print("Reference log-files match!")
         return
-    import shlex
     def qp( p ):
         return shlex.quote(str(p.absolute()))
     raise SystemExit(f"""
@@ -71,6 +81,8 @@ def main( ):
         if not reflogfile.is_file():
             raise SystemExit(f'Reference log file not found: {reflogfile}')
     run(app_file, reflogfile)
+    sys.stdout.flush()
+    sys.stderr.flush()
 
 if __name__=='__main__':
     main()
