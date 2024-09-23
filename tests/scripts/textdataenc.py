89d56143b93e7f4e3e0940b6d35eb97c7cb97d97
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-09-23 12:18:34 +0200

    update encoding test

--- tests/scripts/textdataenc.py
+++ tests/scripts/textdataenc.py
@@ -25,12 +25,66 @@ import NCrystal as NC
 # A very simple test, based on something that gave problems on Windows (due to
 # encodings or line endings):
 
-td = NC.createTextData('stdlib::Ti_sg194.ncmat')
+def query_print( key ):
+    lines=[]
+    print()
+    print( "===============================================================" )
+    print( "===============================================================" )
+    print( "===============================================================" )
+    print( f"=== NC.createTextData( {repr(key)} ===" )
+    td = NC.createTextData(key)
+    print( "=== Printing line by line ===" )
+    for line in td:
+        lines.append(line)
+        print(line)
+    print( "=============================" )
+    assert not '\r' in td.rawData
+    print( "=== Printing .rawData ===" )
+    print( td.rawData, end='' )
+    print( "=============================" )
+    return lines,td.rawData
 
-print( "=== Printing line by line ===" )
-for line in td:
-    print(line)
-print( "=============================" )
-print( "=== Printing .rawData ===" )
-print( td.rawData, end='' )
-print( "=============================" )
+query_print('stdlib::Ti_sg194.ncmat')
+
+dummy_data = [
+    b'NCMAT v7',
+    b'@DENSITY',
+    b'  1 g_per_cm3',
+    b'@DYNINFO',
+    b'  element  C',
+    b'  fraction 1',
+    b'  type     vdosdebye',
+    b'  debye_temp 300',
+]
+
+dummy_data = b'<ENDOFLINE>'.join( dummy_data+[b''] )
+
+dummy_data_decoded = dummy_data.decode('ascii').replace('<ENDOFLINE>','\n')
+NC.registerInMemoryFileData( 'unix.ncmat',
+                             dummy_data.replace(b'<ENDOFLINE>', b'\x0a' ) )
+NC.registerInMemoryFileData( 'dos.ncmat',
+                             dummy_data.replace(b'<ENDOFLINE>', b'\x0d\x0a' ) )
+NC.registerInMemoryFileData( 'mixed.ncmat',
+                             dummy_data
+                             .replace( b'@DENSITY<ENDOFLINE>',
+                                       b'@DENSITY\x0d\x0a')
+                             .replace( b'<ENDOFLINE>',
+                                       b'\x0a' )
+                            )
+NC.registerInMemoryFileData( 'weird.ncmat',
+                             dummy_data
+                             .replace( b'@DENSITY<ENDOFLINE>',
+                                       b'@DENSITY\x0d')
+                             .replace( b'<ENDOFLINE>',
+                                       b'\x0a' )
+                            )
+
+res_unix =  query_print('virtual::unix.ncmat')
+res_dos = query_print('virtual::dos.ncmat')
+res_mixed = query_print('virtual::mixed.ncmat')
+res_weird = query_print('virtual::weird.ncmat')
+
+for enc in ('unix','dos','mixed','weird'):
+    lines,rawdata = query_print(f'virtual::{enc}.ncmat')
+    assert lines == dummy_data_decoded.splitlines(), f'Unexpected {enc}.ncmat lines'
+    assert rawdata == dummy_data_decoded, f'Unexpected {enc}.ncmat .rawData'
