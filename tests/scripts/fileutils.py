30e6e4d78c0c57e88441e45887a2f3bec90fe0bc
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-09-29 17:38:59 +0200

    First stab at new NCFileUtils test

---
+++ tests/scripts/fileutils.py
@@ -0,0 +1,19 @@
+
+from NCTestUtils.loadlib import Lib
+lib = Lib('testfileutils')
+
+import pathlib
+d = pathlib.Path('.')
+(d / 'foo.txt').write_text('foo')
+
+assert lib.nctest_file_exists('./foo.txt')
+assert lib.nctest_file_exists('foo.txt')
+assert not lib.nctest_file_exists('bar.txt')
+assert not lib.nctest_file_exists('./bar.txt')
+assert not lib.nctest_file_exists('/some/file/that/does/not/exist')
+assert d.samefile ( lib.nctest_ncgetcwd() )
+
+#print('cwd',lib.nctest_ncgetcwd())
+
+#FIXME: Much more, including globbing and whatever is likely to cause issues on
+#Windows.
