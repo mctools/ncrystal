abd64e9b477918e72d2f5944378a59da0dbea287
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-09-29 19:15:22 +0200

    Block exceptions in extern C test methods

--- tests/shlibs/lib_testfileutils/lib.cc
+++ tests/shlibs/lib_testfileutils/lib.cc
@@ -1,7 +1,7 @@
 #include <cstring>
 #include "NCrystal/NCDefs.hh"
 #include "NCrystal/internal/NCFileUtils.hh"
-#include <iostream>
+
 namespace NC = NCrystal;
 
 extern "C" const char * nctest_ctypes_dictionary()
@@ -12,20 +12,45 @@ extern "C" const char * nctest_ctypes_dictionary()
     ;
 }
 
+//////////////////////////////////////////////////////
+//FIXME: To common test header:
+#include <iostream>
+#include <cstdlib>
+
+namespace nctest {
+  inline void printErrAndExit(const std::exception &e) noexcept(true) {
+
+      const std::runtime_error* stdrte = dynamic_cast<const std::runtime_error*>(&e);
+      if (stdrte)
+        std::cout<<"NCTest ERROR (std::runtime_error): "<<stdrte->what()<<std::endl;
+      else
+        std::cout<<"NCTest ERROR (unknown)"<<std::endl;
+      std::exit(1);
+  }
+}
+#define NCCATCH catch (std::exception& e) { nctest::printErrAndExit(e); }
+//////////////////////////////////////////////////////
+
+
 extern "C" int nctest_file_exists( const char * f )
 {
-  nc_assert_always(f);
-  std::cout<<"TKTEST "<<f<<" -> "<<NC::file_exists( f )<<std::endl;
-  return NC::file_exists( f ) ? 1 : 0;
+  int res;
+  try {
+    nc_assert_always( f );
+    res = NC::file_exists( f ) ? 1 : 0;
+  } NCCATCH;
+  return res;
 }
 
 extern "C" const char * nctest_ncgetcwd()
 {
   //For testing, we can get away with just using a large static buffer:
-  std::string thecwd = NC::ncgetcwd();
   static char buf[16384];
-  buf[0] = '\0';
-  std::strncat(buf,thecwd.c_str(),sizeof(buf)-1);
+  try {
+    std::string thecwd = NC::ncgetcwd();
+    buf[0] = '\0';
+    std::strncat(buf,thecwd.c_str(),sizeof(buf)-1);
+  } NCCATCH;
   return &buf[0];
 }
 
