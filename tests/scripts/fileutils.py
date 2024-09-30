294137946e2dec915ab8b7da612cff5672034db8
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-09-30 10:07:31 +0200

    More work on fileutils and testing infrastructure

--- tests/scripts/fileutils.py
+++ tests/scripts/fileutils.py
@@ -1,6 +1,7 @@
 
 from NCTestUtils.loadlib import Lib
 lib = Lib('testfileutils')
+lib.dump()
 assert hasattr(lib,'nctest_file_exists')
 assert hasattr(lib,'nctest_ncgetcwd')
 
@@ -13,7 +14,15 @@ assert lib.nctest_file_exists('foo.txt')
 assert not lib.nctest_file_exists('bar.txt')
 assert not lib.nctest_file_exists('./bar.txt')
 assert not lib.nctest_file_exists('/some/file/that/does/not/exist')
-assert d.samefile ( lib.nctest_ncgetcwd() )
+print("Testing that two files are identical:")
+_cwd = lib.nctest_ncgetcwd()
+d_abs = d.resolve().absolute()
+assert d.samefile(d_abs)
+print("  1) %s"%d_abs)
+print("  2) %s"%_cwd)
+assert d_abs.samefile ( pathlib.Path(_cwd).resolve().absolute() )
+assert d_abs.samefile ( _cwd )
+assert d.samefile ( _cwd )
 
 #print('cwd',lib.nctest_ncgetcwd())
 
