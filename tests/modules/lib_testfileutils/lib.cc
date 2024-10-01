66c3893dd63ca7a3bd2f96a5785d9a1be809cbf2
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-10-01 11:44:15 +0200

    Common header-only include path for test modules

--- tests/modules/lib_testfileutils/lib.cc
+++ tests/modules/lib_testfileutils/lib.cc
@@ -19,42 +19,11 @@
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "NCrystal/NCDefs.hh"
+#include "NCTestUtils/NCTestModUtils.hh"
 #include "NCrystal/internal/NCFileUtils.hh"
 #include <cstring>
 
-namespace NC = NCrystal;
-
-//////////////////////////////////////////////////////
-//FIXME: To common test header:
-#if defined (_WIN32) || defined (__CYGWIN__) || defined (WIN32)
-#  define NCTEST_API __declspec(dllexport)
-#elif defined(__GNUC__) || defined(__clang__)
-#  define NCTEST_API __attribute__ ((visibility ("default")))
-#else
-#  define NCTEST_API
-#endif
-#define NCTEST_CTYPES extern "C" NCTEST_API
-
-#include <iostream>
-#include <cstdlib>
-#include <stdexcept>
-
-namespace nctest {
-  inline void printErrAndExit(const std::exception &e) noexcept(true) {
-    const std::runtime_error* stdrte = dynamic_cast<const std::runtime_error*>(&e);
-    if (stdrte)
-      std::cout<<"NCTest ERROR (std::runtime_error): "<<stdrte->what()<<std::endl;
-    else
-      std::cout<<"NCTest ERROR (unknown)"<<std::endl;
-    std::exit(1);
-  }
-}
-#define NCCATCH catch (std::exception& e) { nctest::printErrAndExit(e); }
-//////////////////////////////////////////////////////
-
-
-NCTEST_CTYPES const char * nctest_ctypes_dictionary()
+NCTEST_CTYPE_DICTIONARY
 {
   return
     "int nctest_file_exists( const char * );"
@@ -64,7 +33,7 @@ NCTEST_CTYPES const char * nctest_ctypes_dictionary()
 
 NCTEST_CTYPES int nctest_file_exists( const char * f )
 {
-  int res;
+  int res = -1;
   try {
     nc_assert_always( f );
     res = NC::file_exists( f ) ? 1 : 0;
