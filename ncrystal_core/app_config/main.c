65d0b40ffbdc837ecfa47bdc636ca305bc00c8e9
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2024-11-11 13:53:10 +0100

    Update CMakeLists.txt files for new repo layout

---
+++ ncrystal_core/app_config/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+  (void)argc;
+  (void)argv;
+  //Fixme: replace with proper app.
+  printf("Hello I am ncrystal-config\n");
+  return 0;
+}
