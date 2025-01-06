9e03c569a39bc1e09d11ad3e80cf737d93ff8a1a
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-01-06 19:46:40 +0100

    Update license blurbs

--- examples/plugin/src/NCPluginBoilerPlate.cc
+++ examples/plugin/src/NCPluginBoilerPlate.cc
@@ -1,4 +1,24 @@
 
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  This file is part of NCrystal (see https://mctools.github.io/ncrystal/)   //
+//                                                                            //
+//  Copyright 2015-2024 NCrystal developers                                   //
+//                                                                            //
+//  Licensed under the Apache License, Version 2.0 (the "License");           //
+//  you may not use this file except in compliance with the License.          //
+//  You may obtain a copy of the License at                                   //
+//                                                                            //
+//      http://www.apache.org/licenses/LICENSE-2.0                            //
+//                                                                            //
+//  Unless required by applicable law or agreed to in writing, software       //
+//  distributed under the License is distributed on an "AS IS" BASIS,         //
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  //
+//  See the License for the specific language governing permissions and       //
+//  limitations under the License.                                            //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
 //Define exported symbols used by NCrystal to activate the plugin. This is
 //mostly generic, except for the registerPlugin function which can potentially
 //be modified in some use-cases.
