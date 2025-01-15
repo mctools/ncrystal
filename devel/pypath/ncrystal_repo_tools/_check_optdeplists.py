0ba8dfad31b1065114d9785d45c66d2da3028e60
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-01-15 15:47:12 +0100

    Generate devel/reqs/requirement_<name>.txt files based on ncrystal_metapkg/pyproject.toml dependencies

--- devel/pypath/ncrystal_repo_tools/_check_optdeplists.py
+++ devel/pypath/ncrystal_repo_tools/_check_optdeplists.py
@@ -26,11 +26,19 @@
 #all = [ 'ase<3.24.0', 'gemmi', 'spglib', 'matplotlib' ]
 #devel = [ 'simple-build-system', 'ruff', 'cppcheck', 'mpmath', 'tomli', 'pybind11' ]
 
+def get_base_deps():
+    from .dirs import reporoot
+    from .toml import parse_toml
+    t1 = parse_toml( reporoot.joinpath('ncrystal_python/pyproject.toml') )
+    t2 = parse_toml( reporoot.joinpath('ncrystal_python/pyproject.toml') )
+    return set( t1['project']['dependencies'] + t2['project']['dependencies'])
+
 def get_master_source():
     from .dirs import reporoot
     from .toml import parse_toml
     frel = 'ncrystal_metapkg/pyproject.toml'
     t = parse_toml( reporoot.joinpath(frel) )
+    assert len(t['project']['dependencies'])==2#just ncrystal-* pkgs
     t = t['project']['optional-dependencies']
 
     #Make sure the same dependency is listed with same version reqs everywhere:
@@ -61,12 +69,40 @@ def get_master_source():
 def produce_pipreqfile( deplist ):
     return '\n'.join(sorted(set(deplist)))+'\n'
 
+def check_pipreqfiles(src):
+    #FIXME: Missing global deps like numpy, which we should dig out of ncrystal_core+ncrystal_python. Also, add all_plus_devel with both combined?
+    base_deps = get_base_deps()
+    from .dirs import reporoot
+    files = {}
+    for k,v in src.items():
+        frel = f'devel/reqs/requirement_{k}.txt'
+        assert not frel in files
+        files[frel] = produce_pipreqfile( set(v + list(base_deps)) )
+    def print_expected_content(frel):
+        print('-------- Expected content ------------')
+        print(files[frel],end='')
+        print('--------------------------------------')
+    found = set()
+    for f in reporoot.joinpath('devel','reqs').glob('requirement_*.txt'):
+        frel = str(f.relative_to(reporoot))
+        if not frel in files:
+            raise SystemExit(f'ERROR: Excess file: {frel}')
+        if not files[frel]==f.read_text():
+            print(f'ERROR: Unexpected content in file: {frel}')
+            print_expected_content(frel)
+            raise SystemExit(1)
+        found.add(frel)
+    for frel in files:
+        if not frel in found:
+            print(f'ERROR: Missing file: {frel}')
+            print_expected_content(frel)
+            raise SystemExit(1)
+        print(f'  Verified {frel}')
+
 def main():
     src = get_master_source()
     #Fixme: actually produce these files (+conda files) in devel/envs:
-    for k,v in src.items():
-        print(k)
-        print(produce_pipreqfile(v))
+    check_pipreqfiles(src)
 
 if __name__=='__main__':
     main()
---
+++ devel/reqs/README.md
@@ -0,0 +1,4 @@
+The requirements_<name>.txt files here correspond to the optional dependencies
+of the ncrystal meta package in `ncrystal_metapkg/pyproject.toml`, with the
+addition of any non-ncrystal dependencies needed by the ncrystal-core and
+ncrystal-python packages (meaning in practice just numpy).
