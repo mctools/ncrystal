c0788e6eb666fbc17984315fa59c94b80111bbbf
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-01-24 09:26:52 +0100

    Try to support bash-completions and zsh in devel/setup.sh

--- devel/setup.sh
+++ devel/setup.sh
@@ -1,2 +1,16 @@
-#!/bin/bash
-export PATH="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/bin:${PATH}"
+if [ "x${BASH_VERSION:-}" != "x" ]; then
+    export PATH="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/bin:${PATH}"
+    type complete compgen 1>/dev/null 2>/dev/null
+    if [ $? == 0 ]; then
+        _ncdevtool_completions ()
+        {
+            COMPREPLY=($(compgen -W "$(ncdevtool --show-completion-list)" "${COMP_WORDS[1]}"))
+        }
+        complete -F _ncdevtool_completions ncdevtool
+    fi
+elif [ "x${ZSH_VERSION:-}" != "x" ]; then
+    export PATH="${0:a:h}/bin:${PATH}"
+else
+    echo "ERROR: This script only supports bash or zsh for now. You will"
+    echo "       have to manually add <reporoot>/devel/bin to your PATH"
+fi
