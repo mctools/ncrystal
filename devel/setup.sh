895ad6d6d9e6f4aecfa158899c517799312e05f0
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-01-27 08:59:51 +0100

    Fix ncdevtool bash completions

--- devel/setup.sh
+++ devel/setup.sh
@@ -1,12 +1,21 @@
+#Support bash and zsh, and enable ncdevtool bash completions on first keyword,
+#in case of bash4 (i.e. not osx shipped bash):
+
 if [ "x${BASH_VERSION:-}" != "x" ]; then
     export PATH="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/bin:${PATH}"
-    type complete compgen 1>/dev/null 2>/dev/null
-    if [ $? == 0 ]; then
-        _ncdevtool_completions ()
-        {
-            COMPREPLY=($(compgen -W "$(ncdevtool --show-completion-list)" "${COMP_WORDS[1]}"))
-        }
-        complete -F _ncdevtool_completions ncdevtool
+    if ((BASH_VERSINFO >= 4)); then
+        type complete compgen 1>/dev/null 2>/dev/null
+        if [ $? == 0 ]; then
+            _ncdevtool_completions ()
+            {
+                if [ $COMP_CWORD -eq 1 ]; then
+                    COMPREPLY=($(compgen -W "$(ncdevtool --show-completion-list)" "${COMP_WORDS[1]}"))
+                else
+                    COMPREPLY=()
+                fi
+            }
+            complete -o bashdefault -o default -F _ncdevtool_completions ncdevtool
+        fi
     fi
 elif [ "x${ZSH_VERSION:-}" != "x" ]; then
     export PATH="${0:a:h}/bin:${PATH}"
