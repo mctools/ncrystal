4c623b39f869bf1c19224cc4d624e8cdf4d7ee36
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-04-23 21:10:37 +0200

    Also add functions for Sabine El and Eb

--- tests/scripts/extinc.py
+++ tests/scripts/extinc.py
@@ -25,14 +25,12 @@
 from NCTestUtils.loadlib import Lib
 import mpmath
 mp = mpmath.mp
-mp.dps = 100
+mp.dps = 200
 mpf = mp.mpf
 import NCTestUtils.enable_fpe # noqa F401
 
 lib = Lib('extinction')
 lib.dump()
-assert hasattr(lib,'nctest_calcSabineA')
-assert hasattr(lib,'nctest_calcSabineB')
 
 def ref_calcSabineA(y):
     y = mpf(y)
@@ -47,24 +45,61 @@ def ref_calcSabineB(y):
     #Evaluate 1/y - exp(-y)/sinh(y) for y>=0
     return mpf(1)/y - mp.exp(-y)/mp.sinh(y)
 
-def cmp( fct, reffct, x, fctname ):
-    y = mpf(fct(x))
-    print(f"   {fctname}({float(x):.14g}) = {float(y):.14g}")
-    yr = mpf(reffct(x))
+def ref_calcSabineEb(x,y):
+    x = mpf(x)
+    y = mpf(y)
+    A = ref_calcSabineA( y )
+    B = ref_calcSabineB( y )
+    return A / mp.sqrt( mpf(1) + B * x )
+
+def ref_calcSabineEl(x,y):
+    x = mpf(x)
+    y = mpf(y)
+    if x <= mpf(1):
+        k = ( mpf(1)-(x/mpf(2))+((x**2)/mpf(4))
+              -((x**3)*mpf(5)/mpf(48.))+((x**4)*mpf(7)/mpf(192)) )
+    else:
+        k = ( mp.sqrt( mpf(2)/(mp.pi*x))*
+              ( mpf(1)
+                - mpf(1)/(mpf(8)*x)
+                - mpf(3)/(mpf(128)*(x**2))
+                - mpf(15) / (mpf(1024)*(x**3)) )
+             )
+    return k * mp.exp( -y )
+
+def cmp( fct, reffct, x, fctname, x2 = None ):
+    def fmt(v):
+        return f'{float(v):.14g}'
+    argstr = fmt(x) if x2 is None else '%s, %s'%(fmt(x), fmt(x2))
+    args = (x,) if x2 is None else (x,x2)
+    y = mpf(fct(*args))
+    print(f"   {fctname}({argstr}) = {fmt(y)}")
+    yr = mpf(reffct(*args))
     if not mpmath.almosteq(y,yr, rel_eps=1e-15, abs_eps=1e-99):
         rd = y/yr - mpf(1)
-        raise SystemExit(f'Function produced wrong output at argument {x}.'
-                         f' Got {y} but expected {yr} (reldiff: {float(rd):e})')
+        raise SystemExit(f'Function produced wrong output at argument ({argstr}).'
+                         f' Got {fmt(y)} but expected {fmt(yr)} (reldiff: {float(rd):e})')
 
 def testAB():
     yvals = [ 0.0, 1e-99, 1e-20, 1e-3, 0.1,
               0.2-1e-3, 0.2, 0.2+1e-3,
               0.3-1e-3, 0.3, 0.3+1e-3,
               0.3, 0.7, 0.9, 20, 1e3, 1e5, 1e10, 1e50, 1e99, 1e198, 1e300 ]
+    xvals = [ 0.0, 1e-99, 0.1, 0.5, 0.999999, 1.0, 1.0000001, 10.0, 1e50,
+              1e300 ]
+
     for y in yvals:
         cmp(lib.nctest_calcSabineA, ref_calcSabineA,y,"calcSabineA" )
     for y in yvals:
         cmp(lib.nctest_calcSabineB, ref_calcSabineB,y,"calcSabineB" )
+
+    for y in yvals:
+        for x in xvals:
+            cmp(lib.nctest_calcSabineEb, ref_calcSabineEb,x,"calcSabineEb", x2 = y)
+
+    for y in yvals:
+        for x in xvals:
+            cmp(lib.nctest_calcSabineEl, ref_calcSabineEl,x,"calcSabineEl", x2 = y)
 
 def main():
     testAB()
