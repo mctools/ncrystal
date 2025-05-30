e016b352a0e98558891213b8eff40a7817bf8a6e
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-05-30 16:44:04 +0200

    Avoid hiding repeated warnings during tests by appending [warn#%i] string to msgs

--- tests/pypath/NCTestUtils/reprint_escaped_warnings.py
+++ tests/pypath/NCTestUtils/reprint_escaped_warnings.py
@@ -23,13 +23,26 @@
 Catch escaped warnings and print them without a stack trace (since stack traces
 tend to make test output reproducible).
 
+Additionally, use the NCrystalDev._common._add_warn_counts_to_msgs hook to
+ensure that any NCrystal warnings gets a running warning count appended to the
+message, to prevent the warning framework from silencing repeated warnings.
+
 """
 
 __all__=[]
 
 import warnings
+import NCrystalDev._common as _c
+
+_c._add_warn_counts_to_msgs[0] = True
+
 def printWarnings( message, category, *args, **kwargs ):
     import NCrystalDev._common as nc_common
     msg,cat = nc_common.WarningSpy._fmtwarning( message, category )
     print(f'CAUGHT ESCAPED WARNING ({cat}): {msg}')
+
 warnings.showwarning = printWarnings
+
+del _c
+del warnings
+del printWarnings
