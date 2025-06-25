5edcbdd493d0797958ada8873cdae753e1c8e51d
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-06-25 13:53:13 +0200

    extn: Add extn types to be seen in general cfg interfaces

---
+++ ncrystal_core/include/NCrystal/internal/cfgutils/NCCfgExtn.hh
@@ -0,0 +1,86 @@
+#ifndef NCrystal_CfgExtn_hh
+#define NCrystal_CfgExtn_hh
+
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  This file is part of NCrystal (see https://mctools.github.io/ncrystal/)   //
+//                                                                            //
+//  Copyright 2015-2025 NCrystal developers                                   //
+//                                                                            //
+//  Licensed under the Apache License, Version 2.0 (the "License");           //
+//  you may not use this file except in compliance with the License.          //
+//  You may obtain a copy of the License at                                   //
+//                                                                            //
+//      http://www.apache.org/licenses/LICENSE-2.0                            //
+//                                                                            //
+//  Unless required by applicable law or agreed to in writing, software       //
+//  distributed under the License is distributed on an "AS IS" BASIS,         //
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  //
+//  See the License for the specific language governing permissions and       //
+//  limitations under the License.                                            //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+#include "NCrystal/internal/cfgutils/NCCfgTypes.hh"
+
+namespace NCRYSTAL_NAMESPACE {
+
+  /////////////////////////////////////////////////////////////////
+  // Infrastructure needed for configuration of extinction mdels //
+  /////////////////////////////////////////////////////////////////
+
+  namespace Cfg {
+
+    namespace Extn {
+
+      //To maximize flexibility, extinction configuration is passed around
+      //opaguely in a VarBuf encoded by a CfgKeyValMap. This can be converted to
+      //and from either cfg-string value, or model-specific data
+      //structures. BadInput exceptions are used everywhere in case of invalid
+      //input (like bad cfg-string syntax or inconsistent parameters).
+
+      CfgKeyValMap decode_cfgstr( VarId, StrView );
+      void stream_to_cfgstr( std::ostream&, const CfgKeyValMap& );
+
+      ExtnCfg createExtnCfgFromVarBuf( VarBuf&& );
+      const VarBuf& accessInternalVarBuf( const ExtnCfg& );
+
+      ////////////////////////////////////////////////
+      // General parameters for any model:
+
+      enum class Model { Sabine };
+
+      struct ExtnCfg_Base {
+        Model model;
+        Length domainSize;
+        struct Grain {
+          Length grainSize;
+          double angularSpread;//spread of domains inside a grain
+        };
+        Optional<Grain> grain;
+        static ExtnCfg_Base decode( const CfgKeyValMap& );
+      };
+
+      ////////////////////////////////////////////////
+      // Additional parameters for specific models:
+
+      struct ExtnCfg_Sabine {
+        enum class Tilt { Rectangular, Triangular };
+        enum class Correlation { Correlated, Uncorrelated };
+        Tilt tilt = Tilt::Rectangular;
+        Correlation correlation = Correlation::Correlated;
+        static ExtnCfg_Sabine decode( const CfgKeyValMap& );
+      };
+
+
+      ////////////////////////////////////////////////
+      // Stream adapters:
+      std::ostream& operator<<(std::ostream&, const ExtnCfg_Base& );
+      std::ostream& operator<<(std::ostream&, const ExtnCfg_Sabine& );
+
+    }
+
+  }
+}
+
+#endif
