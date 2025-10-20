fb17020cf511d4da168342ff9001d0a9f6c47ee2
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-10-20 15:30:05 +0200

    Add more dedicated unit tests for NCMATComposer

---
+++ tests/scripts/composer.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+################################################################################
+##                                                                            ##
+##  This file is part of NCrystal (see https://mctools.github.io/ncrystal/)   ##
+##                                                                            ##
+##  Copyright 2015-2025 NCrystal developers                                   ##
+##                                                                            ##
+##  Licensed under the Apache License, Version 2.0 (the "License");           ##
+##  you may not use this file except in compliance with the License.          ##
+##  You may obtain a copy of the License at                                   ##
+##                                                                            ##
+##      http://www.apache.org/licenses/LICENSE-2.0                            ##
+##                                                                            ##
+##  Unless required by applicable law or agreed to in writing, software       ##
+##  distributed under the License is distributed on an "AS IS" BASIS,         ##
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  ##
+##  See the License for the specific language governing permissions and       ##
+##  limitations under the License.                                            ##
+##                                                                            ##
+################################################################################
+
+import NCrystalDev as NC
+from NCTestUtils.common import ensure_error
+import pathlib
+
+def main():
+    print('\n\n  ================> He Ne gas example\n\n')
+
+    c_gas = NC.NCMATComposer()
+    c_gas.set_dyninfo_freegas( 'He', fraction = 0.91 )
+    c_gas.set_dyninfo_freegas( 'Ne', fraction = 0.09 )
+    c_gas.set_state_of_matter( 'gas' )#to be expressive, this line not strictly needed
+    c_gas.set_density(0.233642,'kg/m3')
+
+    c_gas_ncmat = c_gas()
+    print(c_gas_ncmat)
+
+    c_gas.load().dump()
+    c_gas.write('mysillygas.ncmat')
+    assert pathlib.Path('mysillygas.ncmat').read_text() == c_gas_ncmat
+
+    scatter = NC.createScatter("mysillygas.ncmat;temp=50K")
+    scatter.dump()
+
+    c_gas.register_as('mysillygas2.ncmat')
+    scatter = NC.createScatter("mysillygas2.ncmat;temp=50K")
+    scatter.dump()
+
+
+    print('\n\n  ================> silly PE example\n\n')
+
+    c_PE = NC.NCMATComposer()
+    #help(c_PE.set_dyninfo_msd)
+    c_PE.set_dyninfo_msd('H',msd=0.022,temperature=50,fraction=2/3)
+    c_PE.set_dyninfo_msd('C',msd=0.0076,temperature=50,fraction=1/3)
+    c_PE.set_density(0.92,'g/cm3')
+    print(c_PE.create_ncmat())
+
+
+    print('\n\n  ================> U example\n\n')
+
+    c_u = NC.NCMATComposer("solid::U/1gcm3")#note: can also init from a cfg-string!
+    c_u.set_composition('U','U232')
+    with ensure_error(NC.NCBadInput,
+                      'Atom with label "U232" is unknown. If it is a valid'
+                      ' isotope which is simply missing in NCrystal\'s internal'
+                      ' database you must define it yourself.'):
+        c_u.load()
+
+
+    c_u.update_atomdb('U232','232.04u -12.3fm 4.5b 6.789b')#NB: dummy data values!
+    print(c_u())
+    c_u.load().dump()
+    #alternative syntax:
+    print(c_u())
+    c_u.update_atomdb('U232',mass=232.04, coh_scat_len=-12.3, incoh_xs=4.5, abs_xs=6.789)
+    c_u.load().dump()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/scripts/composer2.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+################################################################################
+##                                                                            ##
+##  This file is part of NCrystal (see https://mctools.github.io/ncrystal/)   ##
+##                                                                            ##
+##  Copyright 2015-2025 NCrystal developers                                   ##
+##                                                                            ##
+##  Licensed under the Apache License, Version 2.0 (the "License");           ##
+##  you may not use this file except in compliance with the License.          ##
+##  You may obtain a copy of the License at                                   ##
+##                                                                            ##
+##      http://www.apache.org/licenses/LICENSE-2.0                            ##
+##                                                                            ##
+##  Unless required by applicable law or agreed to in writing, software       ##
+##  distributed under the License is distributed on an "AS IS" BASIS,         ##
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  ##
+##  See the License for the specific language governing permissions and       ##
+##  limitations under the License.                                            ##
+##                                                                            ##
+################################################################################
+
+# NEEDS: spglib
+import NCrystalDev as NC
+from NCTestUtils.common import ensure_error
+
+def main():
+    print('\n\n  ================> Al example 1\n\n')
+
+    c_Al = NC.NCMATComposer()
+    c_Al.set_cellsg_cubic( 4.05 )
+    c_Al.set_atompos( [ ('Al',0,0,0),
+                        ('Al',0,1/2,1/2),
+                        ('Al',1/2,0,1/2),
+                       ('Al',1/2,1/2,0)])
+    c_Al.allow_fallback_dyninfo()
+    with ensure_error(NC.NCBadInput,
+                      "Must provide a space group number (or invoke"
+                      " .refine_crystal_structure()) before it is"
+                      " possible to verify a crystal structure"):
+        c_Al.create_ncmat()
+
+    c_Al.refine_crystal_structure()
+
+    print( c_Al() )
+    c_Al.load().dump()
+
+    print('\n\n  ================> Al example 2\n\n')
+    c_Al2 = NC.NCMATComposer()
+    c_Al2.set_cellsg_cubic( 4.05, spacegroup=225 )
+    c_Al2.set_atompos( [('Al',0,0,0),('Al',0,1/2,1/2),('Al',1/2,0,1/2),('Al',1/2,1/2,0)])
+    c_Al2.allow_fallback_dyninfo()
+    c_Al2.set_composition('Al','0.99 Al 0.01 Cr')
+    print(c_Al2())
+
+    print('\n\n  ================> Al example 3\n\n')
+
+    c_Al3 = NC.NCMATComposer()
+    c_Al3.set_cellsg_cubic( 4.05 )
+    c_Al3.set_atompos( [('tight_atom',0,0,0),('loose_atom',0,1/2,1/2),('loose_atom',1/2,0,1/2),('loose_atom',1/2,1/2,0)])
+    c_Al3.set_dyninfo_msd('tight_atom',msd=0.005, temperature=200)
+    c_Al3.set_dyninfo_msd('loose_atom',msd=0.02, temperature=200)
+    c_Al3.set_composition('tight_atom','Al')
+    c_Al3.set_composition('loose_atom','Al')
+    c_Al3.refine_crystal_structure()#Detect spacegroup
+    print(c_Al3())
+
+    print('\n\n  ================> Al SANS example\n\n')
+
+    c=NC.NCMATComposer('Al_sg225.ncmat') #<--- Can init from cfg-string.
+    c.add_secondary_phase(0.01,'void.ncmat')
+    c.add_hard_sphere_sans_model(50)
+    print(c())
+    c.load().dump()
+
+
+
+if __name__ == '__main__':
+    main()
