322321558025e19d5af94df97332dde7b137c20a
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-11-21 09:20:16 +0100

    Add first unit test monitoring MiniMC output for statistical significant changes

---
+++ tests/scripts/mmc_al.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+################################################################################
+##                                                                            ##
+##  This file is part of NCrystal (see https://mctools.github.io/ncrystal/)   ##
+##                                                                            ##
+##  Copyright 2015-2025 NCrystal developers                                   ##
+##                                                                            ##
+##  Licensed under the Apache License, Version 2.0 (the "License");           ##
+##  you may not use this file except in compliance with the License.          ##
+##  You may obtain a copy of the License at                                   ##
+##                                                                            ##
+##      http://www.apache.org/licenses/LICENSE-2.0                            ##
+##                                                                            ##
+##  Unless required by applicable law or agreed to in writing, software       ##
+##  distributed under the License is distributed on an "AS IS" BASIS,         ##
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  ##
+##  See the License for the specific language governing permissions and       ##
+##  limitations under the License.                                            ##
+##                                                                            ##
+################################################################################
+
+# NEEDS: numpy
+
+def main():
+    from NCTestUtils.minimc_ref import main_minimc_unittest_stdsphere as m
+    m( cfgstr='Al_sg225.ncmat',neutron_energy='4.0Aa', key='<auto>_4Aa' )
+    m( cfgstr='Al_sg225.ncmat',neutron_energy='1.0Aa', key='<auto>_0.8Aa' )
+
+
+
+if __name__ == '__main__':
+    main()
