a0427315392bb4e8491ebcc77f6f35bf1ffe8fb1
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-12-11 07:37:34 +0100

    MiniMC sources now provide nominal beam energy and direction for tallies

--- ncrystal_core/src/minimc/NCMMC_Source.cc
+++ ncrystal_core/src/minimc/NCMMC_Source.cc
@@ -197,6 +197,16 @@ namespace NCRYSTAL_NAMESPACE {
           nc_assert(m_w>0.0&&std::isfinite(m_w));
         }
 
+        Optional<NeutronDirection> nominalBeamDirection() const override
+        {
+          return NullOpt;//no such thing!
+        }
+
+        Optional<NeutronEnergy> nominalBeamEnergy() const override
+        {
+          return m_ekin;
+        }
+
         void toJSONDecodedCfgItems(std::ostream& os) const
         {
           sourceJSONHelper_namexyzwn(os,"isotropic",m_x,m_y,m_z,m_w,m_stat);
@@ -289,6 +299,15 @@ namespace NCRYSTAL_NAMESPACE {
           nc_assert(m_w>0.0&&std::isfinite(m_w));
         }
 
+        Optional<NeutronDirection> nominalBeamDirection() const override
+        {
+          return m_dir;
+        }
+
+        Optional<NeutronEnergy> nominalBeamEnergy() const override
+        {
+          return m_ekin;
+        }
 
         void toJSONDecodedCfgItems(std::ostream& os) const
         {
@@ -415,6 +434,16 @@ namespace NCRYSTAL_NAMESPACE {
                                <<m_center<<", dir="<<m_dir<<", ekin="<<m_ekin
                                <<", r="<<m_radius
                                <<", a="<<m_a<<", b="<<m_b);
+        }
+
+        Optional<NeutronDirection> nominalBeamDirection() const override
+        {
+          return m_dir;
+        }
+
+        Optional<NeutronEnergy> nominalBeamEnergy() const override
+        {
+          return m_ekin;
         }
 
         void toJSONDecodedCfgItems(std::ostream& os) const
