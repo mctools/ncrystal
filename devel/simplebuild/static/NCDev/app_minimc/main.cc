13de97cf9c92ebab70bcbc2b677d01941f1e6295
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2026-04-10 18:19:36 +0200

    Use factory threads in sb_ncdev_minimc

--- devel/simplebuild/static/NCDev/app_minimc/main.cc
+++ devel/simplebuild/static/NCDev/app_minimc/main.cc
@@ -19,26 +19,48 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include "NCrystal/internal/minimc/NCMMC_Query.hh"
+#include "NCrystal/internal/utils/NCString.hh"
+#include "NCrystal/threads/NCFactThreads.hh"
 #include <iostream>
 #include <fstream>
 
+namespace NC = NCrystal;
+
 int main( int argc, char** argv )
 {
-  if ( argc!=6 ) {
+  if ( argc!=6 && argc!=7 ) {
     std::cout
-      <<"Please provide args: [cfgstr] [geomcfg] [srccfg] [enginecfg] [outfile]"
+      <<"Please provide args: [cfgstr] [geomcfg] [srccfg] [enginecfg] [outfile] [factory_nthreads]"
       <<std::endl;
     std::cout
       <<"(outfile should be a json file, \"none\" or \"stdout\")."
+      <<std::endl;
+    std::cout
+      <<"(factory_threads should be \"auto\", \"off\" or an integer value - default is \"auto\")."
       <<std::endl;
     return 1;
   }
   const std::string dest(argv[5]);
 
+  std::string fact_threads("auto");
+  if ( argc==7 )
+    fact_threads = argv[6];
+  if ( fact_threads == "off" ) {
+    //do nothing
+  } else if ( fact_threads == "auto" ) {
+    NC::FactoryThreadPool::enable();
+  } else {
+    auto nt = NC::str2int32( fact_threads,
+                             "factory threads should be auto/off/<integer>");
+    nc_assert_always(nt>=0&&nt<=9999);
+    NC::FactoryThreadPool
+      ::enable(NC::ThreadCount(static_cast<std::uint32_t>(nt)));
+  }
+
   std::string res;
   {
     //Invoke MiniMC:
-    NCrystal::MiniMC::Query::Query query;
+    NC::MiniMC::Query::Query query;
     query.emplace_back("mmc");
     query.emplace_back("run");
     query.emplace_back(argv[1]);
@@ -46,7 +68,7 @@ int main( int argc, char** argv )
     query.emplace_back(argv[3]);
     query.emplace_back(argv[4]);
     std::ostringstream tmp;
-    NCrystal::MiniMC::Query::JSONQuery( tmp, query );
+    NC::MiniMC::Query::JSONQuery( tmp, query );
     res = std::move(tmp).str();
   }
 
