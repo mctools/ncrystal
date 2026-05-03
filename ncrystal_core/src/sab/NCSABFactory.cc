7ef8e1a3416abd1ac727977acb0bc463c4a13fad
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2026-05-03 14:19:29 +0200

    Cleanup and improve static treatment in NCSABFactory.cc

--- ncrystal_core/src/sab/NCSABFactory.cc
+++ ncrystal_core/src/sab/NCSABFactory.cc
@@ -27,40 +27,74 @@ namespace NC = NCrystal;
 
 namespace NCRYSTAL_NAMESPACE {
   namespace SAB {
+    namespace {
 
-    //Cache key is (sabdata uid, egrid uid, sabdata ptr):
-    //TODO: we should use new thin-key support instead of these
-    //shared_obj<const NC::SABData>* pointers!
-    typedef std::tuple<UniqueIDValue,UniqueIDValue,shared_obj<const NC::SABData>*> ScatHelperCacheKey;
-
-    class ScatterHelperFactory
-      : public NC::CachedFactoryBase<ScatHelperCacheKey,
-                                     SABScatterHelper,
-                                     20/*NStrongRefsKept*/> {
-    public:
-      const char* factoryName() const final { return "ScatterHelperFactory"; }
-      std::string keyToString( const ScatHelperCacheKey& key ) const final
+      UniqueIDValue emptyEgridUIDVal()
       {
-        std::ostringstream ss;
-        ss<<"(SABData id="<<std::get<0>(key).value<<";egrid id="<<std::get<1>(key).value<<")";
-        return ss.str();
+        static NC::UniqueID uid_empty;
+        return uid_empty.getUniqueID();
       }
-    protected:
-      virtual ShPtr actualCreate( const ScatHelperCacheKey& key ) const final
-      {
-        auto sabdata_shptr = *std::get<2>(key);
-        nc_assert( sabdata_shptr->getUniqueID() == std::get<0>(key) );
-        auto egrid_shptr = egridFromUniqueID(std::get<1>(key));
-        return createScatterHelper(std::move(sabdata_shptr),std::move(egrid_shptr));
-      }
-    };
 
-    static ScatterHelperFactory s_scathelperfact;
+      //Cache key is (sabdata uid, egrid uid, sabdata ptr):
+      //TODO: we should use new thin-key support instead of these
+      //shared_obj<const NC::SABData>* pointers!
+      typedef std::tuple<UniqueIDValue,
+                         UniqueIDValue,
+                         shared_obj<const NC::SABData>*> ScatHelperCacheKey;
+
+      class ScatterHelperFactory
+        : public NC::CachedFactoryBase<ScatHelperCacheKey,
+                                       SABScatterHelper,
+                                       20/*NStrongRefsKept*/> {
+      public:
+        const char* factoryName() const final { return "ScatterHelperFactory"; }
+        std::string keyToString( const ScatHelperCacheKey& key ) const final
+        {
+          std::ostringstream ss;
+          ss<<"(SABData id="<<std::get<0>(key).value
+            <<";egrid id="<<std::get<1>(key).value<<")";
+          return ss.str();
+        }
+      protected:
+        virtual ShPtr actualCreate( const ScatHelperCacheKey& key ) const final
+        {
+          auto sabdata_shptr = *std::get<2>(key);
+          nc_assert( sabdata_shptr->getUniqueID() == std::get<0>(key) );
+          auto egrid_shptr = egridFromUniqueID(std::get<1>(key));
+          return createScatterHelper(std::move(sabdata_shptr),
+                                     std::move(egrid_shptr));
+        }
+      };
+
+      ScatterHelperFactory& getScatterHelperFactory()
+      {
+        static ScatterHelperFactory s_scathelperfact;
+        return s_scathelperfact;
+      }
+
+      //Egrid UID cache. NB: We never clean this particular cache in order to
+      //preserve the id's, in case something somewhere is still hanging on to
+      //one of them after a cache clearance.
+      struct EgridUIDCacheDB {
+        std::map< HashValue,
+                  std::vector<std::pair<std::shared_ptr<const VectD>,
+                                        UniqueID>>> egridHashCache;
+        std::map< uint64_t, std::shared_ptr<const VectD>* > uid2egrid;
+        std::mutex mtx;
+      };
+
+      EgridUIDCacheDB& getEgridUIDCacheDB()
+      {
+        static EgridUIDCacheDB db;
+        return db;
+      }
+    }
   }
 }
 
-std::unique_ptr<const NC::SAB::SABScatterHelper> NC::SAB::createScatterHelper( shared_obj<const NC::SABData> data,
-                                                                               std::shared_ptr<const VectD> energyGrid )
+std::unique_ptr<const NC::SAB::SABScatterHelper>
+NC::SAB::createScatterHelper( shared_obj<const NC::SABData> data,
+                              std::shared_ptr<const VectD> energyGrid )
 {
   nc_assert(!!data);
   SABIntegrator si(data,energyGrid.get());
@@ -69,11 +103,12 @@ std::unique_ptr<const NC::SAB::SABScatterHelper> NC::SAB::createScatterHelper( s
 }
 
 void NC::SAB::clearScatterHelperCache() {
-  s_scathelperfact.cleanup();
+  getScatterHelperFactory().cleanup();
 }
 
-NC::shared_obj<const NC::SAB::SABScatterHelper> NC::SAB::createScatterHelperWithCache( shared_obj<const NC::SABData> dataptr,
-                                                                                       std::shared_ptr<const VectD> egrid )
+NC::shared_obj<const NC::SAB::SABScatterHelper>
+NC::SAB::createScatterHelperWithCache( shared_obj<const NC::SABData> dataptr,
+                                       std::shared_ptr<const VectD> egrid )
 {
   nc_assert_always(!!dataptr);
 
@@ -81,26 +116,21 @@ NC::shared_obj<const NC::SAB::SABScatterHelper> NC::SAB::createScatterHelperWith
                           egridToUniqueID( egrid ),
                           &dataptr );
 
-  return s_scathelperfact.create(key);
-}
-
-namespace NCRYSTAL_NAMESPACE {
-  namespace {
-    //Egrid UID cache. NB: We never clean this particular cache in order to
-    //preserve the id's, in case something somewhere is still hanging on to one
-    //of them after a cache clearance.
-    static std::map< HashValue, std::vector<std::pair<std::shared_ptr<const VectD>, UniqueID>>> s_egridHashCache;
-    static std::map< uint64_t, std::shared_ptr<const VectD>* > s_uid2egrid;
-    static std::mutex s_egrid2uid_mutex;
-  }
+  return getScatterHelperFactory().create(key);
 }
 
 NC::UniqueIDValue NC::SAB::egridToUniqueID(const NC::VectD& egrid)
 {
+  if ( egrid.empty() ) {
+    //empty => no need for hashing or locking
+    return emptyEgridUIDVal();
+  }
+
   //NB: code duplicated from here to following function
   auto hash = hashContainer(egrid);
-  NCRYSTAL_LOCK_GUARD(s_egrid2uid_mutex);
-  auto& v = s_egridHashCache[hash];//In absence of hash collisions, v will have length 0 or 1.
+  auto& db = getEgridUIDCacheDB();
+  NCRYSTAL_LOCK_GUARD(db.mtx);
+  auto& v = db.egridHashCache[hash];//In absence of hash collisions, v will have length 0 or 1.
   for (auto& e : v) {
     if ( *e.first == egrid )
       return e.second.getUniqueID();//exists in cache already
@@ -108,19 +138,22 @@ NC::UniqueIDValue NC::SAB::egridToUniqueID(const NC::VectD& egrid)
   //Add new:
   v.emplace_back(std::make_shared<const VectD>(egrid),UniqueID() );
   auto uidval = v.back().second.getUniqueID();
-  s_uid2egrid[uidval.value] = &v.back().first;
+  db.uid2egrid[uidval.value] = &v.back().first;
   return uidval;
 }
 
 NC::UniqueIDValue NC::SAB::egridToUniqueID(const std::shared_ptr<const NC::VectD>& egrid)
 {
-  if (!egrid)
-    return egridToUniqueID(VectD());//treat nullptr as empty grid.
+  if ( !egrid || egrid->empty() ) {
+    //Treat nullptr as empty grid and handle specially.
+    return emptyEgridUIDVal();
+  }
 
   //code duplicated here from preceding function
   auto hash = hashContainer(*egrid);
-  NCRYSTAL_LOCK_GUARD(s_egrid2uid_mutex);
-  auto& v = s_egridHashCache[hash];//In absence of hash collisions, v will have length 0 or 1.
+  auto& db = getEgridUIDCacheDB();
+  NCRYSTAL_LOCK_GUARD(db.mtx);
+  auto& v = db.egridHashCache[hash];//In absence of hash collisions, v will have length 0 or 1.
   for (auto& e : v) {
     if ( *e.first == *egrid )
       return e.second.getUniqueID();//exists in cache already
@@ -128,18 +161,20 @@ NC::UniqueIDValue NC::SAB::egridToUniqueID(const std::shared_ptr<const NC::VectD
   //Add new:
   v.emplace_back( egrid, UniqueID() );
   auto uidval = v.back().second.getUniqueID();
-  s_uid2egrid[uidval.value] = &v.back().first;
+  db.uid2egrid[uidval.value] = &v.back().first;
   return uidval;
 }
 
-
-std::shared_ptr<const NC::VectD> NC::SAB::egridFromUniqueID( NC::UniqueIDValue uidval )
+std::shared_ptr<const NC::VectD>
+NC::SAB::egridFromUniqueID( NC::UniqueIDValue uidval )
 {
-  NCRYSTAL_LOCK_GUARD(s_egrid2uid_mutex);
-  auto it = s_uid2egrid.find(uidval.value);
-  if ( it == s_uid2egrid.end() )
-    NCRYSTAL_THROW(LogicError,"egridFromUniqueID passed uid which was not created by call to egridToUniqueID");
+  if ( uidval == emptyEgridUIDVal() )
+    return nullptr;
+  auto& db = getEgridUIDCacheDB();
+  NCRYSTAL_LOCK_GUARD(db.mtx);
+  auto it = db.uid2egrid.find(uidval.value);
+  if ( it == db.uid2egrid.end() )
+    NCRYSTAL_THROW(LogicError,"egridFromUniqueID passed uid which was not"
+                   " created by call to egridToUniqueID");
   return *it->second;
-
 }
-//fixme: we have now duplicated the egridFromUniqueID feature!
