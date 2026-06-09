3105ebc96b70232f26ce4a2e2bb5104a259bd3c3
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2026-06-09 12:35:26 +0200

    Improve SABCellSurvey logic slightly

--- ncrystal_core/src/sab/NCSABSurveyor.cc
+++ ncrystal_core/src/sab/NCSABSurveyor.cc
@@ -29,8 +29,8 @@ namespace NCRYSTAL_NAMESPACE {
   namespace SABUtils {
     namespace {
 
-      bool rectIntersectsIdentityLine(double x0, double y0,
-                                      double x1, double y1) {
+      inline bool rectIntersectsIdentityLine( double x0, double y0,
+                                              double x1, double y1 ) {
         //Branchless efficient check whether or not the axis-aligned rectangle
         //with corners at (x0,y0) and (x1,y1) intersects the line y=x.
         nc_assert(x1>x0);
@@ -69,8 +69,6 @@ NCS::SABSurveyor::SABSurveyor( const VectD& alphaGrid,
   const idx_t nalpha = static_cast<idx_t>(na_sizet);
   const idx_t nbeta  = static_cast<idx_t>(nb_sizet);
   const auto ncells_sizet = (na_sizet-1)*(nb_sizet-1);
-  m_touch.reserve( ncells_sizet );
-  m_cover.reserve( ncells_sizet );
   m_data.reserve( ncells_sizet );
   //The first energy value E at which a given point in (alpha,beta) space is
   //accessible is what we must find for all grid points in order to answer
@@ -132,7 +130,7 @@ NCS::SABSurveyor::SABSurveyor( const VectD& alphaGrid,
       const double e_prevab = *itPAEBUF;
       const double e_preva = *std::next(itPAEBUF);
       double etouch;
-     if ( bval < 0.0 && valueInInterval( aval_prev, aval, -bval ) )
+      if ( bval < 0.0 && valueInInterval( aval_prev, aval, -bval ) )
         etouch = -bval;
       else
         etouch = ( rectIntersectsIdentityLine(bval_prev, aval_prev,
@@ -140,8 +138,6 @@ NCS::SABSurveyor::SABSurveyor( const VectD& alphaGrid,
                    ? 0.0
                    : ncmin( e,e_prevb,e_prevab,e_preva) );
       const double ecover = ncmax( e_prevb,e_prevab,e_preva);
-      m_touch.emplace_back( etouch, packidx );
-      m_cover.emplace_back( ecover, packidx );
       m_data.emplace_back( etouch, ecover, packidx );
 
       *itPAEBUF++ = e_prevb;
@@ -153,12 +149,21 @@ NCS::SABSurveyor::SABSurveyor( const VectD& alphaGrid,
     aval_prev = aval;
   }
 
-  nc_assert( m_touch.size() == ncells_sizet );
-  nc_assert( m_cover.size() == ncells_sizet );
   nc_assert( m_data.size() == ncells_sizet );
-  std::sort( m_touch.begin(), m_touch.end() );
-  std::sort( m_cover.begin(), m_cover.end() );
   std::sort( m_data.begin(), m_data.end() );
+
+  //fixme: the following lines seem innocent, but they cost >70ms or so!!!!
+  m_touch.reserve( ncells_sizet );
+  for ( auto& cc : m_data )
+    m_touch.emplace_back( cc.e_touch, cc.cellidx );
+  nc_assert( m_touch.size() == ncells_sizet );
+  std::sort( m_touch.begin(), m_touch.end() );
+
+  m_cover.reserve( ncells_sizet );
+  for ( auto& cc : m_data )
+    m_cover.emplace_back( cc.e_cover, cc.cellidx );
+  nc_assert( m_cover.size() == ncells_sizet );
+  std::sort( m_cover.begin(), m_cover.end() );
 }
 
 NCS::SABCellSurvey::SABCellSurvey( double alpha1, double alpha2,
@@ -191,7 +196,7 @@ NCS::SABCellSurvey::SABCellSurvey( double alpha1, double alpha2,
   if (b2 <= -e)
     return;//no overlap
   const double twoe = e+e;
-  double tmp = 2*std::sqrt(e*(b2+e));
+  const double tmp = 2*std::sqrt(e*(b2+e));
   const double ap2 = twoe+b2+tmp;//alpha^+(b2)
   if ( a1 >= ap2 )
     return;//no overlap
@@ -200,8 +205,9 @@ NCS::SABCellSurvey::SABCellSurvey( double alpha1, double alpha2,
   double am1(-1.0), ap1(-1.0);
   if ( b1 >= -e ) {
     const double tmp2 = 2*std::sqrt(e*(b1+e));
-    am1 = 2*e+b1-tmp2;//alpha^-(b1)
-    ap1 = 2*e+b1+tmp2;//alpha^+(b1)
+    const double twoe_plus_b1 = twoe+b1;
+    am1 = twoe_plus_b1-tmp2;//alpha^-(b1)
+    ap1 = twoe_plus_b1+tmp2;//alpha^+(b1)
   }
 
   //snap along alpha:
@@ -216,25 +222,29 @@ NCS::SABCellSurvey::SABCellSurvey( double alpha1, double alpha2,
 
   struct Interval final {
     Interval( double aa1, double aa2, bool bb )
-      : a1(aa1), a2(aa2), bounded(bb) {}
+      : a1(aa1), a2(aa2), bounded(bb) { nc_assert( a2 > a1 ); }
     double a1, a2;
     bool bounded;
   };
   //fixme: Can we do it without smallvector?
   SmallVector<Interval,3> intervals_lower, intervals_upper;
+
   //Look at lower bounds:
   if ( b1 <= -e ) {
     intervals_lower.emplace_back(a1,a2,true);//bounded by beta^-(alpha)
   } else {
     double aa1 = a1;
     nc_assert(am1!=-1.0);
-    if ( aa1 < am1 ) {
-      intervals_lower.emplace_back(aa1,am1,true);//bounded by beta^-(alpha)
-      aa1 = am1;
+    const double ncmin_am1_a2 = ncmin(am1,a2);
+    if ( aa1 < ncmin_am1_a2 ) {
+      intervals_lower.emplace_back(aa1,
+                                   ncmin_am1_a2,true);//bounded by beta^-(alpha)
+      aa1 = ncmin_am1_a2;
     }
-    if ( aa1 < ap1 ) {
-      intervals_lower.emplace_back(aa1,ncmin(ap1,a2),false);//bounded by b1 edge
-      aa1 = ncmin(ap1,a2);
+    const double ncmin_ap1_a2 = ncmin(ap1,a2);
+    if ( aa1 < ncmin_ap1_a2 ) {
+      intervals_lower.emplace_back(aa1,ncmin_ap1_a2,false);//bounded by b1 edge
+      aa1 = ncmin_ap1_a2;
     }
     if ( aa1 < a2 )
       intervals_lower.emplace_back(aa1,a2,true);//bounded by beta^-(alpha)
@@ -244,9 +254,10 @@ NCS::SABCellSurvey::SABCellSurvey( double alpha1, double alpha2,
     am2 = 0.0;
   {
     double aa1 = a1;
-    if ( aa1 < am2 ) {
-      intervals_upper.emplace_back(aa1,ncmin(am2,a2),true);//bounded by beta^+(alpha)
-      aa1 = ncmin(am2,a2);
+    const double ncmin_am2_a2 = ncmin(am2,a2);
+    if ( aa1 < ncmin_am2_a2 ) {
+      intervals_upper.emplace_back(aa1,ncmin_am2_a2,true);//bounded by beta^+(alpha)
+      aa1 = ncmin_am2_a2;
     }
     if ( aa1 < a2 )
       intervals_upper.emplace_back(aa1,a2,false);//bounded by b2 edge
@@ -258,9 +269,11 @@ NCS::SABCellSurvey::SABCellSurvey( double alpha1, double alpha2,
   nc_assert( intervals_lower.front().a1 == intervals_upper.front().a1 );
   nc_assert( intervals_lower.back().a2 == intervals_upper.back().a2 );
   double au = intervals_upper.back().a2, al;
+
   while ( !intervals_lower.empty() ) {
     bool bm_bounded =  intervals_lower.back().bounded;
     bool bp_bounded =  intervals_upper.back().bounded;
+    nc_assert_always(!intervals_upper.empty());
     if ( intervals_lower.back().a1 >= intervals_upper.back().a1 ) {
       al = intervals_lower.back().a1;
       intervals_lower.pop_back();
@@ -268,20 +281,26 @@ NCS::SABCellSurvey::SABCellSurvey( double alpha1, double alpha2,
       al = intervals_upper.back().a1;
       intervals_upper.pop_back();
     }
-    m_regions.emplace_back();
-    m_regions.back().alpha_low = al;
-    m_regions.back().alpha_up = au;
-    m_regions.back().is_bounded_by_betaminus = bm_bounded;
-    m_regions.back().is_bounded_by_betaplus = bp_bounded;
+    if ( au > al ) {
+      m_regions.emplace_back();
+      nc_assert(au > al);
+      m_regions.back().alpha_low = al;
+      m_regions.back().alpha_up = au;
+      m_regions.back().is_bounded_by_betaminus = bm_bounded;
+      m_regions.back().is_bounded_by_betaplus = bp_bounded;
+    }
     au = al;
   }
   nc_assert( intervals_upper.size()==1 );
   const auto nsize = m_regions.size();
   nc_assert( nsize <= nmax_regions );
 
+  if ( nsize == 0 )
+    return;
+
   //Testing with various integration algs indicates that we benefit from more
   //careful splitting around the phasespace endpoints. This should happen very
-  //rarely. Note that we have incremented nmax_regions by 2 due to this!!
+  //rarely. Note that we have incremented nmax_regions by 2 due to this!
   std::size_t isplit = nsize;
   for ( auto i : ncrange( nsize ) ) {
     auto& r = m_regions[i];
@@ -305,14 +324,18 @@ NCS::SABCellSurvey::SABCellSurvey( double alpha1, double alpha2,
   }
 
   //split at endpoint near alpha=0:
-  if ( !m_regions.empty()
-       && m_regions.back().alpha_low < 0.01*m_regions.back().alpha_up ) {
+  const double aal = m_regions.back().alpha_low;
+  const double aau = m_regions.back().alpha_up;
+  if ( aal < 0.01*aau ) {
+    double asplit = 0.05*aau;
+    if ( aal < 0.001*aau )
+      asplit = 0.005*aau;
+    if ( aal < 0.0001*aau )
+      asplit = 0.0005*aau;
     m_regions.push_back(m_regions.back());
-    const double asplit = 0.05*m_regions.back().alpha_up;
     m_regions.back().alpha_up = asplit;
     m_regions.at(m_regions.size()-2).alpha_low = asplit;
   }
-
 }
 
 void NCS::SABCellSurvey::toJSON( std::ostream& os ) const
