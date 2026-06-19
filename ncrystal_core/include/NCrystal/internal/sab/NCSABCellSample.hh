2bd73d45f1053c32220441363bd5ff7791dc6465
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2026-06-19 10:30:17 +0200

    Add BoundedCellSampler which does not have the lowE sampling issue of FullCell-rejection

--- ncrystal_core/include/NCrystal/internal/sab/NCSABCellSample.hh
+++ ncrystal_core/include/NCrystal/internal/sab/NCSABCellSample.hh
@@ -32,9 +32,10 @@ namespace NCRYSTAL_NAMESPACE {
     // Utilities for SAB Cell sampling //
     /////////////////////////////////////
 
-    class LogLinDistSampler {
+    class LogLinDistSampler final {
     public:
 
+      //////////////////////////////////////////////////////////////////////////
       // Class which can be used to sample alpha values according to the usual
       // log-linear alpha interpolation scheme of S(alpha,beta) tables.
       //
@@ -62,9 +63,10 @@ namespace NCRYSTAL_NAMESPACE {
       double m_a, m_b;
     };
 
-    class FullCellSampler : private NoCopyMove  {
+    class FullCellSampler final : private NoCopyMove  {
     public:
 
+      //////////////////////////////////////////////////////////////////////////
       //Sample (alpha,beta) point over a full cell. Note that for efficiency,
       //only a pointer to the CellData object is copied by the constructor, so
       //the .sampleAlphaBeta(..)  method should not be called after the lifetime
@@ -87,6 +89,71 @@ namespace NCRYSTAL_NAMESPACE {
       double m_prob1;
     };
 
+    class BoundedCellSampler final : private NoCopyMove {
+
+      //////////////////////////////////////////////////////////////////////////
+      //Sample (alpha,beta) in the intersection between a given cell and the
+      //kinematic bounds given by the phasespace available to a particular
+      //neutron E/kT.
+
+    public:
+
+      // To construct a BoundedCellSampler object, expensive preprocessing is
+      // required. To facilitate better caching and initialisation performance,
+      // such info is kept in a ProcessedSurveyInfo object which can be created
+      // ahead of time and is optimised for storage in a contiguous data
+      // structure elsewhere. The initialisation is specific to a given neutron
+      // energy, and requires that a SABCellSurvey is already available. All
+      // parameters provided must be identical to those with which the
+      // SABCellSurvey was constructed.
+
+      struct ProcessedSurveyInfo {
+        //Overlay values and (if set), alpha-range in which samples must fall.
+        //Note: overlay values are calculated as S(bmiddle)*bwidth*(b2-b1) + a
+        //      bit of safety.
+        float overlay1, overlay2;
+        float alpha_low;//-1 if not set
+        float alpha_up;//-1 if not set
+      };
+      static ProcessedSurveyInfo
+      processSurveyInfo( const SABCellSurvey&,
+                         double alpha1, double alpha2,
+                         double beta1, double beta2,
+                         double E_div_kT );
+
+      //Initialise. The caller must ensure that all arguments are consistent
+      //(e.g. ProcessedSurveyInfo must be created from the same CellData and
+      //E_div_kT values as those passed along). The probability_b1_edge must be
+      //given by W1/(W1+W2) where W1 is the alpha integral along the beta1 edge
+      //of the cell and W2 the one along the beta2 edge:
+      BoundedCellSampler( const CellData& cell,
+                          const ProcessedSurveyInfo& oi,
+                          double E_div_kT,
+                          double probability_b1_edge );
+
+      //Actual sampling method. For diagnostics purposes, this includes a count
+      //of the internal number of tries (only the last of these was accepted).
+      struct Result {
+        double alpha;
+        double beta;
+        std::uint_fast64_t ntries;
+      };
+      Result sampleAlphaBeta( RNG& );
+
+    private:
+      struct EdgeData {
+        bool islinlin;
+        double s_low, s_up, lns_low, lns_up;
+      };
+      CellData m_cell;
+      Optional<EdgeData> m_edge[2];
+      double m_overlay[2];
+      double m_4e;
+      double m_prob1;
+      double m_restrict_a1;
+      double m_restrict_a2;
+      void initEdge( EdgeData&, unsigned ) const;
+    };
   }
 }
 
@@ -99,7 +166,7 @@ inline double NCrystal::SABUtils::LogLinDistSampler::sample( RNG& rng ) const
   double val;
   if ( m_islinlin ) {
     if ( m_cache2 > m_cache1 && rng.generate()*m_cache2 > m_cache1 ) {
-      val = m_cache3 + m_cache4 * std::sqrt(rng.generate());//fixme: sqrt(R) xvs minmax(R,R)
+      val = m_cache3 + m_cache4 * std::sqrt(rng.generate());
     } else {
       val = m_cache3 + m_cache4 * rng.generate();
     }
@@ -107,6 +174,32 @@ inline double NCrystal::SABUtils::LogLinDistSampler::sample( RNG& rng ) const
     nclikely val = m_a + m_cache1 * randKPowX(m_cache2,m_cache3,rng.generate());
   }
   return ncclamp( val, m_a, m_b );
+}
+
+inline NCrystal::SABUtils::BoundedCellSampler::
+BoundedCellSampler( const CellData& cell,
+               const ProcessedSurveyInfo& psi,
+               double E_div_kT,
+               double probability_b1_edge )
+  : m_cell(cell),
+    m_4e(4.0*E_div_kT),
+    m_prob1(probability_b1_edge),
+    m_restrict_a1(static_cast<double>(psi.alpha_low)),
+    m_restrict_a2(static_cast<double>(psi.alpha_up))
+{
+  nc_assert( m_prob1 >= 0 && m_prob1 <= 1 );
+  nc_assert( E_div_kT > 0 && std::isfinite(E_div_kT) );
+  m_overlay[0] = static_cast<double>(psi.overlay1);
+  m_overlay[1] = static_cast<double>(psi.overlay2);
+  if ( !( m_restrict_a1>0.0 && m_restrict_a1 < cell.a2 ) )
+    m_restrict_a1 = cell.a1;
+  if ( !(m_restrict_a2 > m_restrict_a1 && m_restrict_a2 < cell.a2) )
+    m_restrict_a2 = cell.a2;
+
+  nc_assert( m_overlay[0] > 0.0 );
+  nc_assert( m_overlay[1] > 0.0 );
+  nc_assert( std::isfinite(m_overlay[0]) );
+  nc_assert( std::isfinite(m_overlay[1]) );
 }
 
 #endif
--- ncrystal_core/src/sab/NCSABCellSample.cc
+++ ncrystal_core/src/sab/NCSABCellSample.cc
@@ -20,6 +20,7 @@
 
 #include "NCrystal/internal/sab/NCSABCellSample.hh"
 #include "NCrystal/internal/sab/NCSABUtils.hh"
+#include "NCrystal/internal/utils/NCMsg.hh"
 
 namespace NC = NCrystal;
 namespace NCS = NCrystal::SABUtils;
@@ -107,11 +108,301 @@ NC::PairDD NCS::FullCellSampler::sampleAlphaBeta( RNG& rng )
                          c.a2, c.S[offset+1], c.logS[offset+1] );
   }
   const double alpha = opt_sampler.value().sample(rng);
-  //interpolation along beta is purely linear, so use a triangle which is zero
-  //at the far side:
-  const double rb = ( offset
-                      ? ncmax(rng.generate(),rng.generate())
-                      : ncmin(rng.generate(),rng.generate()) );//fixme: sqrt or minmax trick?
-  const double beta = c.b1*(1.0-rb) + rb*c.b2;
+
+  //interpolation along beta is purely linear, so simply sample a triangle which
+  //is zero at the far side:
+
+  const double t = std::sqrt(rng.generate());
+  const double beta = ( offset
+                        ? ( c.b1*(1.0-t) + t*c.b2 )
+                        : ( c.b1*t + (1.0-t)*c.b2 ) );
   return { alpha, ncclamp(beta, c.b1, c.b2) };
+}
+
+NCS::BoundedCellSampler::Result
+NCS::BoundedCellSampler::sampleAlphaBeta( RNG& rng )
+{
+  //pick side:
+  const unsigned offset = ( rng.generate() <= m_prob1 ? 0 : 2 );
+  auto& optEdge = m_edge[offset/2];
+  if (!optEdge.has_value()) {
+    optEdge.emplace();
+    initEdge(optEdge.value(),offset);
+  }
+  const double overlay_val_orig = m_overlay[offset/2];
+  const auto& edge = optEdge.value();
+  const auto& c = m_cell;
+
+  LogLinDistSampler sampler( m_restrict_a1, edge.s_low, edge.lns_low,
+                             m_restrict_a2, edge.s_up, edge.lns_up,
+                             edge.islinlin );
+
+  Result res;
+  res.ntries = 0;
+  double max_overlay_val_seen = -1.0;
+  double overlay_val = overlay_val_orig;
+  while ( true ) {
+    ++res.ntries;
+#if 1
+    //FIXME: Just testing a way to limit worst performance. Instead of raising
+    //the overlay, we could also simply pick the previous best value after N
+    //tries? (where "best" includes both the actual value and the R value used
+    //for comparison.
+    if ( res.ntries == 200 && max_overlay_val_seen == 0.0 ) {
+      static Msg::WarnFirstTime wf;
+      wf("S(alpha,beta) cell sampling takes unexpectedly long"
+         " to find accepted candidate.");
+      overlay_val = 0.0;
+    }
+    //fixme: just do this at init time (for cells with huge k or 1/k), and lower
+    //cached overlay values? That way we can also use more values (e.g. 100).
+    if ( res.ntries > 20 && max_overlay_val_seen > 0.0 ) {
+      const double new_overlay = (res.ntries>20?1.2:2.0)*max_overlay_val_seen;
+      overlay_val = ncmin(overlay_val_orig,new_overlay);
+    }
+    overlay_val = ncmax(overlay_val,1.001*max_overlay_val_seen);
+#endif
+
+    nc_assert_always( res.ntries < 10000ull );
+    double a = sampler.sample(rng);
+    const double dpmb = std::sqrt(m_4e*a);
+    double bl = a-dpmb;
+    double bu = a+dpmb;
+    double bwidth, bmid;
+    if ( valueInInterval(c.b1,c.b2,bl)
+         && valueInInterval(c.b1,c.b2,bu) ) {
+      bwidth = 2.0*dpmb;
+      bmid = a;
+    } else {
+      bl = ncmax(c.b1,bl);
+      bu = ncmin(c.b2,bu);
+      bwidth = bu-bl;
+      bmid = (bl+bu)*0.5;
+    }
+    if ( !(bwidth>0) )
+      continue;
+    //Now, the relative beta weight starts at 1.0 at the chosen side,
+    //and reduces to 0.0 at the opposite side (due to linear beta
+    //interpolation).
+    double bmidheight_multdb = ( offset ? (bmid-c.b1) : (c.b2-bmid ) );
+    const double actual_val = bwidth*bmidheight_multdb;
+    max_overlay_val_seen = ncmax(max_overlay_val_seen,actual_val);
+    if ( rng.generate() * overlay_val > actual_val )
+      continue;//reject
+
+    //Ok, sample beta.
+    const double hbl = ( offset ? (bl-c.b1) : (c.b2-bl ) );
+    const double hbu = ( offset ? (bu-c.b1) : (c.b2-bu ) );
+    const double hbase = ncmin(hbl,hbu);
+    const double havg = (hbl+hbu)*0.5;
+    double u;
+    if ( rng.generate()*havg <= hbase ) {
+      //base
+      u = rng.generate();
+    } else {
+      //triangle (with highest point in appropriate direction)
+      u = std::sqrt(rng.generate());
+      if ( !offset )
+        u = 1.0 - u;
+    }
+    const double b = ncmin(bu,bl+u*bwidth);
+    nc_assert( ncsquare(a-b) <= a*m_4e*(1.0+1e-10) );
+    res.alpha = a;
+    res.beta = ncclamp( b, c.b1, c.b2 );
+    return res;
+  }
+}
+
+void NCS::BoundedCellSampler::initEdge( EdgeData& edge, unsigned offset ) const
+{
+  const auto& c = m_cell;
+  edge.s_low = c.S[offset];
+  edge.s_up = c.S[offset+1];
+  edge.islinlin = !(ncmin( edge.s_low, edge.s_up )>0.0);
+  if ( edge.islinlin ) {
+    edge.lns_low = edge.lns_up = 0.0;
+    if ( m_restrict_a1 > m_cell.a1 )
+      edge.s_low = interpolate_linlin_NEW(m_cell.a1, c.S[offset],
+                                          m_cell.a2, c.S[offset+1],
+                                          m_restrict_a1);
+    if ( m_restrict_a2 < m_cell.a2 )
+      edge.s_up = interpolate_linlin_NEW(m_cell.a1, c.S[offset],
+                                         m_cell.a2, c.S[offset+1],
+                                         m_restrict_a2);
+  } else {
+    edge.lns_low = c.logS[offset];
+    edge.lns_up = c.logS[offset+1];
+    if ( m_restrict_a1 > m_cell.a1 ) {
+      auto rs = interpolate_loglin_fast2_NEW(m_cell.a1, c.S[offset],
+                                             m_cell.a2, c.S[offset+1],
+                                             m_restrict_a1,
+                                             c.logS[offset],
+                                             c.logS[offset+1]);
+      edge.s_low = rs.first;
+      edge.lns_low = rs.second;
+    }
+
+    if ( m_restrict_a2 < m_cell.a2 ) {
+      auto rs = interpolate_loglin_fast2_NEW(m_cell.a1, c.S[offset],
+                                             m_cell.a2, c.S[offset+1],
+                                             m_restrict_a2,
+                                             c.logS[offset],
+                                             c.logS[offset+1]);
+      edge.s_up = rs.first;
+      edge.lns_up = rs.second;
+    }
+  }
+}
+
+NCS::BoundedCellSampler::ProcessedSurveyInfo
+NCS::BoundedCellSampler::processSurveyInfo( const SABCellSurvey& survey,
+                                            double alpha1, double alpha2,
+                                            double beta1, double beta2,
+                                            double E_div_kT )
+{
+
+  ProcessedSurveyInfo res;
+  res.overlay2 = -1.0f;
+  res.overlay1 = -1.0f;
+  res.alpha_up = -1.0f;
+  res.alpha_low = -1.0f;
+
+  auto& regions = survey.regions();
+  if ( regions.empty() )
+    return res;
+
+  const double b1 = beta1;
+  const double b2 = beta2;
+  const double e = E_div_kT;
+
+  double o1(-1.0), o2(-1.0);
+  auto it = regions.begin();
+  auto itE = regions.end();
+  SABCellSurvey::Region current = *it++;
+  auto updateO1 = [&o1,b2]( double bmid, double bwidth )
+  {
+    o1 = ncmax(o1, (b2-bmid)*bwidth );
+
+  };
+  auto updateO2 = [&o2,b1]( double bmid, double bwidth )
+  {
+    o2 = ncmax(o2, (bmid-b1)*bwidth );
+
+  };
+  auto updateO12 = [&updateO1,&updateO2]( double bmid, double bwidth )
+  {
+    updateO1(bmid,bwidth);
+    updateO2(bmid,bwidth);
+  };
+  auto processCurrent = [b1,b2,e,&current,
+                         &updateO1,&updateO2,&updateO12]()
+  {
+    const auto& r = current;
+    if ( r.is_bounded_by_betaplus ) {
+      if ( r.is_bounded_by_betaminus ) {
+        //bound by [betaminus(alpha),betaplus(alpha)]
+        updateO12( r.alpha_up, 4.0*std::sqrt(e * r.alpha_up) );
+        updateO12( r.alpha_low, 4.0*std::sqrt(e * r.alpha_low) );
+        //local maximum at a=b2/3 for o1 and at a=b1/3 for o2:
+        constexpr double onethird = 1.0/3.0;
+        const double amax1 = b2*onethird;
+        const double amax2 = b1*onethird;
+        if ( valueInInterval( r.alpha_low, r.alpha_up, amax1 ) )
+          updateO1( amax1, 4.0*std::sqrt(e * amax1) );
+        if ( valueInInterval( r.alpha_low, r.alpha_up, amax2 ) )
+          updateO2( amax2, 4.0*std::sqrt(e * amax2) );
+      } else {
+        //bound by [b1,betaplus(alpha)]
+        const double sqrte = std::sqrt(e);
+        const double twosqrte = 2.0 * sqrte;
+        auto pt = [b1,updateO1,updateO2,twosqrte]( double a,
+                                                   bool do1 = true,
+                                                   bool do2 = true ) {
+          const double bplus = a + twosqrte*std::sqrt(a);
+          const double bmid((bplus+b1)*0.5), bwidth(bplus-b1);
+          if (do1)
+            updateO1(bmid,bwidth);
+          if (do2)
+            updateO2(bmid,bwidth);
+        };
+        pt(r.alpha_up);
+        pt(r.alpha_low);
+        const double amax1 = ncsquare( std::sqrt(e+b2)- sqrte );
+        const double amax2 = ncsquare( std::sqrt(e+b1)- sqrte );
+        if ( valueInInterval( r.alpha_low, r.alpha_up, amax1 ) )
+          pt( amax1, true, false );
+        if ( valueInInterval( r.alpha_low, r.alpha_up, amax2 ) )
+          pt( amax2, false, true );
+      }
+    } else {
+      if ( r.is_bounded_by_betaminus ) {
+        //bound by [betaminus(alpha),b2]
+        const double sqrte = std::sqrt(e);
+        const double twosqrte = 2.0 * sqrte;
+        auto pt = [b2,updateO12,twosqrte]( double a ) {
+          const double bminus = a - twosqrte*std::sqrt(a);
+          updateO12((bminus+b2)*0.5,b2-bminus);
+        };
+        pt( r.alpha_up );
+        pt( r.alpha_low );
+        if ( valueInInterval( r.alpha_low, r.alpha_up, e ) )
+          pt( e );
+      } else {
+        //bound by [b1,b2]
+        updateO12( (b1+b2)*0.5, b2-b1 );
+      }
+    }
+  };
+
+  for ( ; it!=itE; ++it ) {
+    if ( current.is_bounded_by_betaminus != it->is_bounded_by_betaminus
+         || current.is_bounded_by_betaplus != it->is_bounded_by_betaplus ) {
+      processCurrent();
+      current = *it;
+    } else {
+      //merge previously split regions:
+      assert( current.alpha_low == it->alpha_up );
+      current.alpha_low = it->alpha_low;
+    }
+  }
+  processCurrent();
+
+  //finish up, rounding single precision floats the right way and add a bit of
+  //safety:
+  auto storeFloat = [](float&dest, double val, bool push_is_up=true)
+  {
+    dest = static_cast<float>( val );
+    if ( push_is_up ) {
+      while ( dest < val )
+        dest = std::nextafter(dest,std::numeric_limits<float>::infinity());
+    } else {
+      while ( dest > val )
+        dest = std::nextafter(dest,-std::numeric_limits<float>::infinity());
+    }
+  };
+
+  if ( regions.front().alpha_up < alpha2 ) {
+    storeFloat(res.alpha_up,regions.front().alpha_up);
+    if ( !(static_cast<double>(res.alpha_up)<alpha2) )
+      res.alpha_up = -1.0f;
+  }
+
+  if ( regions.back().alpha_low > alpha1 ) {
+    storeFloat(res.alpha_low,regions.back().alpha_low,false);
+    if ( !(static_cast<double>(res.alpha_low)>alpha1) )
+      res.alpha_low = -1.0f;
+  }
+
+  storeFloat(res.overlay1,o1);
+  storeFloat(res.overlay2,o2);
+  nc_assert( res.overlay1 >= 0.0f );
+  nc_assert( res.overlay2 >= 0.0f );
+
+  constexpr double overlay_safety_factor = 1.0001;
+  res.overlay1 *= overlay_safety_factor;
+  res.overlay2 *= overlay_safety_factor;
+  nc_assert( res.overlay1 > 0.0f );
+  nc_assert( res.overlay2 > 0.0f );
+
+  return res;
 }
