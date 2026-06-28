1ba18852a9185941ec5e2137bd263d73d47ae7a0
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2026-06-28 11:19:34 +0200

    Improve SAB Cell plotting and mpmath ref utils

--- tests/pypath/NCTestUtils/sabcelleval.py
+++ tests/pypath/NCTestUtils/sabcelleval.py
@@ -98,6 +98,9 @@ class RefCell:
         #Change the global random seed is the only way to reseed
         #mpmath.mp.rand().
         ranges = self.phasespace_integral_ranges(E_div_kT)
+        if len(ranges)==0:
+            #no contribution!
+            return []
         data = dict( alpha = self.__alpha,
                      beta = self.__beta,
                      S = self.__s )
@@ -130,7 +133,7 @@ def draw_alpha_beta_grid(alphagrid,betagrid,**kw_plot):
     axis.set_ylabel('alpha')
     return pctx.finalise( do_grid = False )
 
-def plot_celleval( data, **kw_plot ):
+def plot_celleval( data, do_title=True, **kw_plot ):
     from NCrystalDev.plot import PlotContext
     from NCrystalDev._numpy import _np_linspace
     import numpy as np
@@ -156,6 +159,7 @@ def plot_celleval( data, **kw_plot ):
                 list( v for k,v in data_ci['phasespace_integral']
                       if k=='Romberg33' )[0] ) ]
     for e, lbl, col, integral in elist:
+        assert blim[1]>=-e#revisit this if it fails
         brangeplot = [max(blim[0],-e),blim[1]]
         assert brangeplot[1] > brangeplot[0]
         lble = f'{e:g}kT' if not np.isinf(e) else 'INF'
@@ -199,8 +203,9 @@ def plot_celleval( data, **kw_plot ):
             if color is None:
                 color=_[0].get_color()
 
-    title='s11=%g, s12=%g, s21=%g, s22=%g'%tuple(data['S'])
-    pctx.axis.set_title(title)
+    if do_title:
+        title='s11=%g, s12=%g, s21=%g, s22=%g'%tuple(data['S'])
+        pctx.axis.set_title(title)
     return pctx.finalise( do_grid = False, do_legend='draggable' )
 
 def _mp_integral_k_pow_x(k, a, b,mp):
@@ -225,7 +230,7 @@ def _mp_integral_k_pow_x(k, a, b,mp):
     # The j=1 term is 1 and for the other terms term(j)/term(j-1) = eps/j
     #
     assert abs(eps)<0.5
-    targetprec = mpf(10)**(-mp.mp.dps)
+    targetprec = mpf(10)**(-mp.dps)
     term = mpf(1) # j=1
     totsum = term
     nlim = 2000
@@ -331,17 +336,19 @@ def _ref_integral( data, cellranges, mp, do_sample ):
     (a1, a2), (b1, b2) = data['alpha'], data['beta']
     e, a1, a2, b1, b2 = mpf(e), mpf(a1), mpf(a2), mpf(b1), mpf(b2)
     s11, s12, s21, s22 = (mpf(e) for e in data['S'])
+    sofa_at_b1 = _create_s_of_a(a1,a2,s11,s12,mp)
+    sofa_at_b2 = _create_s_of_a(a1,a2,s21,s22,mp)
+
     totsum = mpf(0)
     if do_sample:
         sampleinfo = dict( e=e, a1=a1, a2=a2, b1=b1, b2=b2,
-                           s11=s11, s12=s12, s21=s21, s22=s22 )
+                           s11=s11, s12=s12, s21=s21, s22=s22)
         sampleregions = []
         sampleinfo['regions'] = sampleregions
+
     for ( r_a1, r_a2, (clip_betaminus, clip_betaplus)) in cellranges['ranges']:
         r_a1, r_a2 = mpf(r_a1), mpf(r_a2)
         assert r_a2 >= r_a1
-        sofa_at_b1 = _create_s_of_a(a1,a2,s11,s12,mp)
-        sofa_at_b2 = _create_s_of_a(a1,a2,s21,s22,mp)
         if do_sample or not (clip_betaminus or clip_betaplus):
             aint_at_b1 = _alphaintegral1d(a1,a2,s11,s12,r_a1,r_a2,mp)
             aint_at_b2 = _alphaintegral1d(a1,a2,s21,s22,r_a1,r_a2,mp)
@@ -349,9 +356,7 @@ def _ref_integral( data, cellranges, mp, do_sample ):
             rangeinfo = ( r_a1, r_a2, (clip_betaminus,clip_betaplus) )
             sampleregions.append( dict( rangeinfo = rangeinfo,
                                         aint_at_b1 = aint_at_b1,
-                                        aint_at_b2 = aint_at_b2,
-                                        sofa_at_b1 = sofa_at_b1,
-                                        sofa_at_b2 = sofa_at_b2 ) )
+                                        aint_at_b2 = aint_at_b2 ) )
 
         if not (clip_betaminus or clip_betaplus):
             #Full box integral of region => no need for numerical quadrature:
@@ -425,7 +430,7 @@ def _find_integration_ranges( E_div_kT, alpha, beta ):
         assert am1 is not None
         assert ap1 is not None
         if _a1 < min(am1,a2):
-            intervals_lower.append( (_a1,min(am1,a2),True) )#bounded by beta^-(alpha)
+            intervals_lower.append( (_a1,min(am1,a2),True) )#bounded by b^-(a)
             _a1 = min(am1,a2)
         if _a1 < min(ap1,a2):
             intervals_lower.append( (_a1,min(ap1,a2),False) )#bounded by b1 edge
@@ -551,7 +556,9 @@ def _sample_impl( E_div_kT, n, rng, mp, refintdata ):
     lin_s_at_b1 = not min(s11,s12)>0
     lin_s_at_b2 = not min(s21,s22)>0
 
-    da = a2 - a1
+    sofa_at_b1 = _create_s_of_a(a1,a2,s11,s12,mp)
+    sofa_at_b2 = _create_s_of_a(a1,a2,s21,s22,mp)
+
     db = b2 - b1
     invdb = 1/db
     e = refintdata['e']
@@ -560,67 +567,65 @@ def _sample_impl( E_div_kT, n, rng, mp, refintdata ):
         if nreg==0:
             continue
         assert reg['contrib'] > 0
+        r_a1, r_a2, (clip_betaminus,clip_betaplus) = reg['rangeinfo']
         W1 = reg['aint_at_b1']
         W2 = reg['aint_at_b2']
-        sofa_at_b1 = reg['sofa_at_b1']
-        sofa_at_b2 = reg['sofa_at_b2']
         fullboxcontrib = (W1+W2)*db/2
         fullboxAR = reg['contrib'] / fullboxcontrib
-        if fullboxAR > 0.1:
-            #Just use the obvious:
-            p_w1 = W1 / (W1+W2)
-            s1,s2 = s11, s12
-            rasampler1 = _gen_ra_sampler(mp,s11,s12,lin_s_at_b1)
-            rasampler2 = _gen_ra_sampler(mp,s21,s22,lin_s_at_b2)
+        if fullboxAR > 0.2:
+            p_w1 = W1 / ( W1 + W2 )
+            rasampler1 = _gen_ra_sampler(mp,
+                                         sofa_at_b1(r_a1),sofa_at_b1(r_a2),
+                                         lin_s_at_b1)
+            rasampler2 = _gen_ra_sampler(mp,
+                                         sofa_at_b2(r_a1),sofa_at_b2(r_a2),
+                                         lin_s_at_b2)
+            ireg = 0
+            while ireg < nreg:
+                is_b1edge = rng()<=p_w1
+                rasampler = rasampler1 if is_b1edge else rasampler2
+                ra = rasampler(rng)
+                a = r_a1*(1-ra)+r_a2*ra
+                #Triangle which has non-zero height at chosen side and 0 at
+                #the far side:
+                rb = min(rng(),rng()) if is_b1edge else max(rng(),rng())
+                b = b1*(1-rb)+b2*rb
+                if (a-b)**2 <= 4*a*e:
+                    ireg += 1
+                    result.append( (a,b) )
+        else:
+
+            #A single box overlay not efficient enough! Sample a based on
+            #contrib_at_a function, using uniform overlay sampling in a number of
+            #cells. Cells granularity is adaptive to high level of change.
+            asampler = AlphaRangeSampler(contrib_at_alpha = reg['contrib_at_a'],
+                                         mp = mp, a1 = r_a1, a2 = r_a2,
+                                         E_div_kT = e, b1=b1, b2=b2)
+            bl,bu = b1, b2
             for i in range(nreg):
-                rasampler = rasampler1 if rng()<p_w1 else rasampler2
+                a = asampler.sample(rng)
+                assert a>=r_a1
+                assert a<=r_a2
+                if clip_betaminus or clip_betaplus:
+                    bwidth = 2*mp.sqrt(e*a)
+                    if clip_betaminus:
+                        bl = max(b1,a-bwidth)
+                    if clip_betaplus:
+                        bu = min(b2,a+bwidth)
+                sb1 = sofa_at_b1(a)
+                sb2 = sofa_at_b2(a)
+                rbl = (bl-b1)*invdb
+                rbu = (bu-b1)*invdb
+                sbl = (1-rbl)*sb1+rbl*sb2
+                sbu = (1-rbu)*sb1+rbu*sb2
                 while True:
-                    #find a:
-                    ra = rasampler(rng)
-                    a = min(a2,a1 + ra*da)
-                    #fixme: option to sample multiple b per a??
-                    s1 = sofa_at_b1(a)
-                    s2 = sofa_at_b2(a)
-                    rbsampler = _gen_triangle01_sampler(mp,s1,s2)
-                    rb = rbsampler(rng)
-                    b = min(b2,b1 + rb*db)
-                    if (a-b)**2 <= 4*a*e:
-                        result.append( (a,b) )
+                    b = bl+rng()*(bu-bl)
+                    rb = (b-b1)*invdb
+                    s = (1-rb)*sb1+rb*sb2
+                    if max(sbl,sbu)*rng()<s:
                         break
-            continue
-
-        #A single box overlay not efficient enough! Sample a based on
-        #contrib_at_a function, using uniform overlay sampling in a number of
-        #cells. Cells granularity is adaptive to high level of change.
-
-        r_a1, r_a2, (clip_betaminus,clip_betaplus) = reg['rangeinfo']
-        asampler = AlphaRangeSampler(contrib_at_alpha = reg['contrib_at_a'],
-                                     mp = mp, a1 = r_a1, a2 = r_a2 )
-        bl,bu = b1, b2
-        for i in range(nreg):
-            a = asampler.sample(rng)
-            assert a>=r_a1
-            assert a<=r_a2
-            if clip_betaminus or clip_betaplus:
-                bwidth = 2*mp.sqrt(e*a)
-                if clip_betaminus:
-                    bl = max(b1,a-bwidth)
-                if clip_betaplus:
-                    bu = min(b2,a+bwidth)
-            sb1 = sofa_at_b1(a)
-            sb2 = sofa_at_b2(a)
-            rbl = (bl-b1)*invdb
-            rbu = (bu-b1)*invdb
-            sbl = (1-rbl)*sb1+rbl*sb2
-            sbu = (1-rbu)*sb1+rbu*sb2
-            while True:
-                b = bl+rng()*(bu-bl)
-                rb = (b-b1)*invdb
-                s = (1-rb)*sb1+rb*sb2
-                if max(sbl,sbu)*rng()<s:
-                    break
-            assert (a-b)**2 <= 4*a*e*(1+mp.mpf(1e-40))
-            result.append( (a,b) )
+                assert (a-b)**2 <= 4*a*e*(1+mp.mpf(1e-40))
+                result.append( (a,b) )
     return result
 
 def _gen_ra_sampler(mp, s1, s2, lin_s):
@@ -669,10 +674,13 @@ def _gen_triangle01_sampler(mp,s1,s2):
 
 class AlphaRangeSampler:
 
-    def __init__(self, mp, contrib_at_alpha, a1, a2 ):
+    def __init__(self, mp, contrib_at_alpha, a1, a2, E_div_kT, b1, b2 ):
         #Initial subdivision:
         a1 = mp.mpf(a1)
         a2 = mp.mpf(a2)
+        b1 = mp.mpf(b1)
+        b2 = mp.mpf(b2)
+        e = mp.mpf(E_div_kT)
         avals = mp.linspace(a1,a2,65)
         f = contrib_at_alpha
         def dev( _f1, _f2 ):
@@ -688,7 +696,8 @@ class AlphaRangeSampler:
             assert f2>=0.0
             cells.append( (dev(f1,f2),f1,f2,ra1,ra2) )
         #Keep subdividing until largest dev is less than devthr:
-        devthr = 0.5
+        devthr = 0.6#tested with 0.4...0.9, all seems ok.
+        ncellslimit = 2000
         while True:
             cells.sort()
             c=cells[-1]
@@ -702,32 +711,59 @@ class AlphaRangeSampler:
             cells.pop()
             cells.append( (dev(f1,fmid),f1,fmid,ra1,ramid) )
             cells.append( (dev(fmid,f2),fmid,f2,ramid,ra2) )
-            assert len(cells)<1000
+            assert len(cells)<ncellslimit
+
+        #special points with possible extrema:
+        special_avals = []
+        mpf = mp.mpf
+        sqrte=mp.sqrt(e)
+        da1 = 2*mp.sqrt(abs(e*(b1+e)))
+        da2 = 2*mp.sqrt(abs(e*(b2+e)))
+        for sa in [ e, b1/mpf(3), b2/mpf(3),
+                    ( mp.sqrt( abs(e + b1) ) - sqrte )**2,
+                    ( mp.sqrt( abs(e + b2) ) - sqrte )**2,
+                    2*e+b1-da1,
+                    2*e+b1+da1,
+                    2*e+b2-da2,
+                    2*e+b2+da2 ]:
+            if a1 < sa < a2:
+                special_avals.append(sa)
+
         #Now, prepare uniform overlay sampler for these cells:
         overlay_contrib = []
         totsum = mp.mpf(0)
+        overlay_vals = []
+        safety = mpf('1.1')
+        finalcells = []
         for c in cells:
             _,f1,f2,ra1,ra2 = c
             assert ra2-ra1 > 0.0
-            assert max(f2,f1) >= 0.0
-            cntb = (ra2-ra1)*max(f2,f1)
+            overlayf = max(f1,f2)
+            assert overlayf >= 0.0
+            for sa in special_avals:
+                if ra1 < sa < ra2:
+                    overlayf = max(overlayf,f(sa))
+            overlayf *= safety
+            cntb = (ra2-ra1)*overlayf
             assert cntb >= 0.0
+            finalcells.append( (overlayf,ra1,ra2) )
             totsum += cntb
             overlay_contrib.append( totsum )
-        self.__cells = cells
+        self.__cells = finalcells
         self.__overlay_contrib = overlay_contrib
         self.__f = f
 
     def sample( self, rng ):
         #Pick cell (bisect_left returns index of first entry not below Rselect):
         from bisect import bisect_left
-        Rselect = rng() * self.__overlay_contrib[-1]
-        idx = bisect_left(self.__overlay_contrib, Rselect)
-        c = self.__cells[idx]
-        _,f1,f2,ra1,ra2 = c
-        overlayf = max(f1,f2)*1.5
-        da = ra2-ra1
         while True:
+            Rselect = rng() * self.__overlay_contrib[-1]
+            idx = bisect_left(self.__overlay_contrib, Rselect)
+            c = self.__cells[idx]
+            overlayf, ra1, ra2 = c
+            da = ra2-ra1
             a = min(ra2,ra1 + rng()*da)
-            if overlayf*rng() <= self.__f(a):
+            fa = self.__f(a)
+            assert fa <= overlayf
+            if overlayf*rng() <= fa:
                 return a
